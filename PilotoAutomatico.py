8c508021c15d2056b4a9cb6514349eac0dd0549a
Sergio Jimenez <anubarakdewaarimasen@gmail.com>
2019-07-19 09:38:05 -0600

    Cambios locales, se utilizara PyAutoGui

--- PilotoAutomatico.py
+++ PilotoAutomatico.py
@@ -1,79 +1,64 @@
+#!usr/bin/python3
 import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
+import pyautogui
 
-#favor borrar mucha mierda cuando no sean las 11 de la noche
-#las pulsaciones de teclado se leen con el script en segundo plano
 #una lectura del teclado sin interrupciones requiere una gran responsabilidad
+#ojalá no se use con fines malignos, nos conocemos lo suficiente
+#y hay gente en 2019 que dice que solo se puede hackear con C o ensamblador
+#HA, HA, HA, las risas
 
-startStop = KeyCode(char='-')#se dispara en true cuando se presiona la tecla
-salida = KeyCode(char=',')#lease comentario anterior
-keyCambiaDelay = KeyCode(char='.')#este ya es por vicio, pero igual que el otro
+keyStartStop = KeyCode(char=',')
+keyConfiguration = KeyCode(char='.')
+keyExit = KeyCode(char='-')
 
-posicion1 = {0,0}
-posicion2 = {0,0}
-delay = 90
-
-print("Instrucciones\n presione - para iniciar repeticion\n \
-	presione . para cambiar el delay general\n \
-	presione , para cerrar el programa")
 class ClickMouse(threading.Thread):
-	def __init__(self, delay):
+	def __init__(self):
 		super(ClickMouse, self).__init__()
-		self.delay = delay
 		self.running = False
 		self.program_running = True
-		self.posicion1 = posicion1
-		self.posicion2 = posicion2
-	def cambiaDelay(self, delay):
-		self.delay = delay
-	def stop_clicking(self):
-		print("*clicking music stop*")
-		self.running = False
+		print("se ha iniciado el hilo")
+	
 	def start_clicking(self):
-		print("*clicking intensifies*")
 		self.running = True
+	
+	def stop_clicking(self):
+		self.running = False
+	
 	def exit(self):
-		print("*clicking band has been killed*")
 		self.stop_clicking()
 		self.program_running = False
-		
+	
 	def run(self):
 		while self.program_running:
-			
 			while self.running:
-				mouse.position = 1399, 227
+				
 				time.sleep(1)
-				mouse.click(Button.right)
-				time.sleep(1)
-				mouse.position = 1449, 278
-				time.sleep(0.5)
-				mouse.click(Button.left)
-				time.sleep(self.delay)
 			time.sleep(0.1)
-                        
-mouse = Controller()
-click_thread = ClickMouse(delay)
+
+class operaciones():
+	def __init__(self, coordinates, action, delay):
+		self.coordinates = [0,0]
+		self.action = 0
+		self.delay = 0
+
+
+click_thread = ClickMouse()
 click_thread.start()
 
-
 def on_press(key):
-    if key == startStop:
-        if click_thread.running:
-            click_thread.stop_clicking()
-        else:
-            click_thread.start_clicking()
-    elif key == salida:
-        click_thread.exit()
-        listener.stop()
-        exit()
-    elif key == keyCambiaDelay:
-        global delay
-        print("Ingrese delay nuevo: ", delay)
-        delay = int(input())
-        click_thread.cambiaDelay(delay)
+	if key == keyStartStop:
+		if click_thread.running:
+			click_thread.stop_clicking()
+		else:
+			click_thread.start_clicking()
+	elif(key == keyConfiguration):
+		#inserteConfiguracionAqui
+	elif key == keyExit:
+		click_thread.exit()
+		listener.stop()
 
 with Listener(on_press=on_press) as listener:
-    listener.join()
-
+	listener.join()

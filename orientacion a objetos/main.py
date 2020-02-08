e67ab3823403a200ab72dac622f1538c26d51947
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-02-08 11:01:05 -0600

    modificado el proyecto de orientacion a objetos para añadir GUI

--- orientacion a objetos/main.py
+++ orientacion a objetos/main.py
@@ -2,26 +2,35 @@
 #coding:utf-8
 
 #del archivo X importamos la clase X como etiqueta X
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import pyqtSlot
+
+#Clases propias
 from Persona import persona as Persona
 from Maestro import maestro as Maestro
 
-def menu():
-	opcion = input("1 - Crear una persona \n2 - Crear un maestro\n3 - salir\n")
-	if(opcion == '1'):
-		crearPersona()
-	elif(opcion == '2'):
-		crearMaestro()
-	else: 
-		exit()	
-
-def crearPersona():
-	laPersona = Persona(input("ingrese el nombre\n"), input("ingrese el apellido\n"), input("ingrese la edad\n"))
-	laPersona.mostrarDatos()
-
-def crearMaestro():
-	elMaestro = Maestro(input("ingrese el nombre\n"), input("ingrese el apellido\n"), input("ingrese la edad\n"), input("ingrese la materia\n"))
-	elMaestro.mostrarDatos()
+@pyqtSlot()
+def crearPersona(self):
+	print("click")
+def salir():
+	exit()
 
 if(__name__ == '__main__'):
-    while(True):
-        menu()
+	raiz = QApplication(sys.argv)
+	ventana = QWidget()
+	ventana.setGeometry(0, 0, 500, 300)
+	ventana.setWindowTitle("Gestor de Personas")
+	
+	btnCrearPersona = QPushButton("CrearPersona", ventana)
+	btnCrearPersona.setToolTip("interfaz para crear persona")
+	btnCrearPersona.move(0,0)
+	btnCrearPersona.clicked.connect(crearPersona)
+	
+	btnSalir = QPushButton("Salir", ventana)
+	btnSalir.setToolTip("salir del programa")
+	btnSalir.move(0, 35)
+	btnSalir.clicked.connect(salir)
+	
+	ventana.show()
+	sys.exit(raiz.exec())

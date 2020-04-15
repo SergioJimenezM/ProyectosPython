09abf8a5731f174f75b7d9612dee99751ec732cc
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-04-14 22:19:58 -0600

    Arreglados los defectos de la tabla

--- ejemploUniversidad/universidad.sql
+++ ejemploUniversidad/universidad.sql
@@ -5,102 +5,115 @@ use Universidad;
 #usa la base de datos universidad para las siguientes consultas
 
 #sintaxis para crear tablas
-create table Personas(
-DNI int,#numeros enteros
-PrimerNombre varchar(20),#texto dinámico que no excede 20 caracteres
-SegundoNombre varchar(20),#se hace mas pequeño quitando el espacio sobrante
-PrimerApellido varchar(20),
-SegundoApellido varchar(20),
-FechaDeNacimiento date,#fecha sin hora
-primary key (DNI)#se define la llave primaria
+CREATE TABLE Personas (
+ DNI int(11) NOT NULL,
+ PrimerNombre varchar(20) NOT NULL,
+ SegundoNombre varchar(20),
+ PrimerApellido varchar(20) NOT NULL,
+ SegundoApellido varchar(20),
+ FechaDeNacimiento date NOT NULL,
+ PRIMARY KEY (DNI)
 );
 
-create table Estado(
-ID int, Estado varchar(20),
-primary key(ID)
+CREATE TABLE Estado (
+ ID int(11) NOT NULL,
+ Estado varchar(20) NOT NULL,
+ PRIMARY KEY (ID)
+) ;
+
+CREATE TABLE Estudiantes (
+ DNIPersona int(11) NOT NULL,
+ CreditosDelCicloLectivo int(11) NOT NULL,
+ CreditosAprobados int(11) NOT NULL,
+ PromedioGeneral decimal(2,1) NOT NULL,
+ Estado int(11) NOT NULL,
+ PRIMARY KEY (DNIPersona),
+ KEY Estado (Estado),
+ CONSTRAINT EstudiantesConPersonas FOREIGN KEY (DNIPersona) REFERENCES Personas (DNI) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT EstudiantesConEstado FOREIGN KEY (Estado) REFERENCES Estado (ID)
 );
 
-create table Estudiantes(
-DNIPersonas int,
-CreditosDelCicloLectivo int,
-CreditosAprobados int,
-PromedioGeneral decimal(2,1),#numeros de 2 digitos y un decimal
-Estado int,
-primary key(DNIPersonas),#la llave primaria es DNI
-foreign key(DNIPersonas) references Personas(DNI),#DNIPersonas es un campo existente en DNI y no uno nuevo
-foreign key(Estado) references Estado(ID)
+CREATE TABLE Profesores (
+ DNIPersonas int(11) NOT NULL,
+ HorasLaboradas int(11) NOT NULL,
+ Estado int(11) NOT NULL,
+ PRIMARY KEY (DNIPersonas),
+ KEY Estado (Estado),
+ CONSTRAINT ProfesoresConPersonas FOREIGN KEY (DNIPersonas) REFERENCES Personas (DNI) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT ProfesoresConEstado FOREIGN KEY (Estado) REFERENCES Estado (ID)
 );
 
-create table Profesores(
-DNIPersonas int,
-HorasLaboradas int, 
-Estado int,
-primary key(DNIPersonas), 
-foreign key(DNIPersonas) references Personas(DNI),
-foreign key(Estado) references Estado(ID)
+CREATE TABLE Telefonos (
+ ID int(11) NOT NULL,
+ Telefonos varchar(15) NOT NULL,
+ PRIMARY KEY (ID)
 );
 
-create table Telefonos(
-ID int,
-Telefonos varchar(15),
-primary key(ID)
+CREATE TABLE PersonasConTelefonos (
+ DNIPersonas int(11) NOT NULL,
+ IDTelefonos int(11) NOT NULL,
+ PRIMARY KEY (DNIPersonas,IDTelefonos),
+ KEY IDTelefonos (IDTelefonos),
+ CONSTRAINT TablaConDNI FOREIGN KEY (DNIPersonas) REFERENCES Personas (DNI) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT TablaConTelefonos FOREIGN KEY (IDTelefonos) REFERENCES Telefonos (ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-create table PersonasConTelefonos(
-DNIPersonas int,
-IDTelefonos int,
-primary key(DNIPersonas, IDTelefonos),
-foreign key(DNIPersonas) references Personas(DNI),
-foreign key(IDTelefonos) references Telefonos(ID)
+CREATE TABLE CorreosElectronicos (
+ ID int(11) NOT NULL,
+ CorreosElectronicos varchar(20) NOT NULL,
+ PRIMARY KEY (ID)
 );
 
-create table CorreosElectronicos(
-ID int,
-CorreosElectronicos varchar(20),
-primary key(ID)
+CREATE TABLE PersonasConCorreos (
+ DNIPersonas int(11) NOT NULL,
+ IDCorreos int(11) NOT NULL,
+ PRIMARY KEY (DNIPersonas,IDCorreos),
+ KEY IDCorreos (IDCorreos),
+ CONSTRAINT TablaConDNI FOREIGN KEY (DNIPersonas) REFERENCES Personas (DNI) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT TablaConCorreos FOREIGN KEY (IDCorreos) REFERENCES CorreosElectronicos (ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-create table PersonasConCorreos(
-DNIPersonas int,
-IDCorreos int,
-primary key(DNIPersonas, IDCorreos),
-foreign key(DNIPersonas) references Personas(DNI),
-foreign key(IDCorreos) references CorreosElectronicos(ID)
+CREATE TABLE EstadoDelCurso (
+ ID int(11) NOT NULL,
+ Estado varchar(20) NOT NULL,
+ PRIMARY KEY (ID)
 );
 
-create table EstadoDelCurso(
-ID int,
-Estado varchar(20)
+CREATE TABLE Cursos (
+ ID int(11) NOT NULL,
+ Sigla varchar(20) NOT NULL,
+ Nombre varchar(20) NOT NULL,
+ Creditos int(11) NOT NULL,
+ PRIMARY KEY (ID),
+ UNIQUE KEY Sigla (Sigla)
 );
 
-create table Cursos(
-ID int,
-Sigla varchar(20),
-Nombre varchar(20),
-Creditos int,
-primary key(ID)
+CREATE TABLE GruposMatriculados (
+ ID int(11) NOT NULL,
+ DNIProfesor int(11) NOT NULL,
+ IDCurso int(11) NOT NULL,
+ Agno year(4) NOT NULL,
+ Dia varchar(8) NOT NULL,
+ HoraDeInicio varchar(5) NOT NULL,
+ DuracionDeLecciones int(11) NOT NULL,
+ PRIMARY KEY (ID,DNIProfesor,IDCurso),
+ KEY DNIProfesor (DNIProfesor),
+ KEY IDCurso (IDCurso),
+ CONSTRAINT GruposMatriculadosConMatriculas FOREIGN KEY (ID) REFERENCES Matriculas (GrupoMatriculado) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT GruposMatriculadosConProfesores FOREIGN KEY (DNIProfesor) REFERENCES Profesores (DNIPersonas) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT GruposMatriculadosConCursos FOREIGN KEY (IDCurso) REFERENCES Cursos (ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-create table GruposMatriculados(
-ID int,
-DNIProfesor int,
-IDCurso int,
-Agno int,
-Dia varchar(8),
-HoraDeInicio varchar(5),
-DuracionDeLecciones int,
-primary key(ID, DNIProfesor, IDCurso),
-foreign key(DNIProfesor) references Profesores(DNIPersonas)
-);
-
-create table Matriculas(
-GrupoMatriculado int,
-DNIEstudiante int,
-Calificacion decimal(2,1),
-EstadoDelCurso int,
-primary key(GrupoMatriculado, DNIEstudiante),
-foreign key(GrupoMatriculado) references GruposMatriculados(ID),
-foreign key (DNIEstudiante) references Estudiantes(DNIPersonas)
+CREATE TABLE Matriculas (
+ GrupoMatriculado int(11) NOT NULL,
+ DNIEstudiante int(11) NOT NULL,
+ Calificacion decimal(2,1) NOT NULL,
+ EstadoDelCurso int(11) NOT NULL,
+ PRIMARY KEY (GrupoMatriculado,DNIEstudiante),
+ KEY DNIEstudiante (DNIEstudiante),
+ KEY EstadoDelCurso (EstadoDelCurso),
+ CONSTRAINT MatriculasConEstudiantes FOREIGN KEY (DNIEstudiante) REFERENCES Estudiantes (DNIPersona) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT MatriculasConEstadoDelCurso FOREIGN KEY (EstadoDelCurso) REFERENCES EstadoDelCurso (ID)
 );
 
 
